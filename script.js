f25c6c7ee716b255b3b0800475d1225509021e95
Nick Poskitt <nick.poskitt@gmail.com>
2021-10-22 21:38:18 +0100

    Added reset button and prompt

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
-const container = document.querySelector('#container');
+const container = document.querySelector('#container'); 
+const reset = document.querySelector('button');
 let rowCount = 0
 let boxCount = 0
-let gridSize = 16
 
+function drawGrid(gridSize) {
 while (rowCount < gridSize) {
     const row = document.createElement('div');
     row.classList.add('row');
@@ -16,11 +17,24 @@ while (rowCount < gridSize) {
     rowCount++
     boxCount = 0
 };
-
+rowCount = 0
 const boxes = document.querySelectorAll('.box')
 boxes.forEach((box => {
     box.addEventListener('mouseover', function() {
         box.classList.add('red');
     });
 }));
+}
 
+drawGrid(16)
+
+function resetGrid() {
+    const rows = document.querySelectorAll('.row')
+    rows.forEach((row => {
+        container.removeChild(row);
+        }))
+    let x = prompt('How many grids would you like to play with?');
+    drawGrid(x)
+    };
+
+reset.addEventListener('click', resetGrid);
